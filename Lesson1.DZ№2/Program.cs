24031d15572d8ce82d623f9e8f1c8354ec403633
gamingtv20181@gmail.com <gamingtv20181@gmail.com>
2020-06-14 10:55:34 +0300

    Добавьте файлы проекта.

---
+++ Lesson1.DZ№2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lesson1.DZ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите название продукта:");
+            string input = Console.ReadLine();
+            Console.WriteLine("Введите количество:");
+            string inputA = Console.ReadLine();
+            float A = float.Parse(inputA.Replace(".", ","));
+            Console.WriteLine("Введите единицу измерения:");
+            string inputB = Console.ReadLine();
+            Console.WriteLine("Введите цену продукта:");
+            string inputC = Console.ReadLine();
+            float C = float.Parse(inputA.Replace(".", ","));
+            Console.Clear();
+            Console.WriteLine("Название продукта: " + input);
+            Console.WriteLine("Количество: " + inputA);
+            Console.WriteLine("Единица измерения: " + inputB);
+            Console.WriteLine("Цена продукта: " + inputC);
+        }
+    }
+}
+            
+           
+       
